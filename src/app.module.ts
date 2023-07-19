626a82bb10f4d6fa5b214249cd369d0cb6f9fd0e
labbaekratul <labbaekratul@gmail.com>
2023-07-19 21:20:18 +0600

    added

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,6 +4,7 @@ import { ConfigModule } from '@nestjs/config';
 import { UserModule } from './libs/user/user.module';
 import { AuthModule } from './libs/auth/auth.module';
 import { DonationModule } from './libs/donation/donation.module';
+import { AppController } from './app.controller';
 
 @Module({
   imports: [
@@ -13,5 +14,6 @@ import { DonationModule } from './libs/donation/donation.module';
     ConfigModule.forRoot({ isGlobal: true }),
     DonationModule,
   ],
+  controllers: [AppController],
 })
 export class AppModule {}
